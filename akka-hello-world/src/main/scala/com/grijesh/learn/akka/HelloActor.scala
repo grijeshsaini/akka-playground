84543e8b10097cc29d341a90536b1339cf3e4d85
grijesh <grijesh.rajoria@gmail.com>
2016-01-24 17:51:59 +0530

    initial project

---
+++ akka-hello-world/src/main/scala/com/grijesh/learn/akka/HelloActor.scala
@@ -0,0 +1,21 @@
+package com.grijesh.learn.akka
+
+import akka.actor.{Props, ActorSystem, Actor}
+
+/**
+  * Created by grijesh on 24/1/16.
+  */
+class HelloActor extends Actor{
+  override def receive = {
+    case "Hey man !!!!" => println("Hey !!!")
+    case _       => println("Have you said something ?")
+  }
+}
+
+object HelloActor extends App{
+  val system = ActorSystem("HelloSystem")
+  // default Actor constructor
+  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  helloActor ! "Hey man !!!!" //tell
+  helloActor ! "hey..."
+}
